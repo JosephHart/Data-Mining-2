59018fbce02e3178cfa35292afcb106edf7dd7e1
joethepro36 <joeisthegreatest@msn.com>
2018-03-28 16:45:23 +0100

    Initial Sub

--- Data Mining - Python/Main.py
+++ Data Mining - Python/Main.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 
 import numpy as np
+from sklearn.cluster import KMeans
 
 #Read file in
 dataArray = np.genfromtxt('HealthScores.csv', delimiter=',', dtype=None).astype(str)
@@ -36,9 +37,17 @@ unitsAlcohol = list(map(int, unitsAlcohol))
 unitsCigs = list(map(int, unitsCigs))
 healthScore = list(map(int, healthScore))
 
+for index, item in enumerate(unitsAlcohol):
+    unitsAlcohol[index] = 0
+
+for index, item in enumerate(unitsCigs):
+    unitsCigs[index] = 0
+
 #setup arrays for loop use
 femaleScores = []
 maleScores = []
+genderNumberScores = []
+
 scores1830 = []
 scores3140 = []
 scores4150 = []
@@ -62,12 +71,29 @@ scoresIQ90 = []
 scoresIQ101 = []
 scoresIQ112 = []
 
+scoresAlcohol0 = []
+scoresAlcohol1 = []
+scoresAlcohol3 = []
+scoresAlcohol5 = []
+
+scoresCigs0 = []
+scoresCigs1 = []
+scoresCigs11 = []
+scoresCigs21 = []
+
+scoresInactive = []
+scoresActive = []
+scoresVeryActive = []
+scoresNumberActivity = []
+
 #extract female and male scores
 for index, item in enumerate(sex):
     if item == "Female":
         femaleScores.append(healthScore[index])
+        genderNumberScores.append(0)
     elif item == "Male":
         maleScores.append(healthScore[index])
+        genderNumberScores.append(1)
         
 #extract age group scores
 for index, item in enumerate(age):
@@ -118,7 +144,40 @@ for index, item in enumerate(iq):
         scoresIQ101.append(healthScore[index])
     elif int(item) >= 112 and int(item) <= 122:
         scoresIQ112.append(healthScore[index])
+        
+#extract drinking group scores
+for index, item in enumerate(unitsAlcohol):
+    if int(item) >= 0 and int(item) <= 0:
+        scoresAlcohol0.append(healthScore[index])
+    elif int(item) >= 1 and int(item) <= 2:
+        scoresAlcohol1.append(healthScore[index])
+    elif int(item) >= 3 and int(item) <= 4:
+        scoresAlcohol3.append(healthScore[index])
+    elif int(item) >= 5 and int(item) <= 7:
+        scoresAlcohol5.append(healthScore[index])
     
+#extract smoking group scores
+for index, item in enumerate(unitsCigs):
+    if int(item) >= 0 and int(item) <= 0:
+        scoresCigs0.append(healthScore[index])
+    elif int(item) >= 1 and int(item) <= 10:
+        scoresCigs1.append(healthScore[index])
+    elif int(item) >= 11 and int(item) <= 20:
+        scoresCigs11.append(healthScore[index])
+    elif int(item) >= 21 and int(item) <= 40:
+        scoresCigs21.append(healthScore[index])
+        
+#extract activity scores
+for index, item in enumerate(activity):
+    if item == "Inactive":
+        scoresInactive.append(healthScore[index])
+        scoresNumberActivity.append(0)
+    elif item == "Active":
+        scoresActive.append(healthScore[index])
+        scoresNumberActivity.append(1)
+    elif item == "Very Active":
+        scoresVeryActive.append(healthScore[index])
+        scoresNumberActivity.append(2)
 
 #print mean and median of results
 print ("Mean of male scores")
@@ -215,8 +274,249 @@ print (np.median(scoresIQ101))
 print ("Median of 112-122 scores")
 print (np.median(scoresIQ112))
 
-print (min(iq))
-print (max(iq))
+print ("Mean of 0-0 scores")
+print (np.mean(scoresAlcohol0))
+print ("Mean of 1-2 scores")
+print (np.mean(scoresAlcohol1))
+print ("Mean of 3-4 scores")
+print (np.mean(scoresAlcohol3))
+print ("Mean of 5-7 scores")
+print (np.mean(scoresAlcohol5))
+
+print ("Median of 0-0 scores")
+print (np.median(scoresAlcohol0))
+print ("Median of 1-2 scores")
+print (np.median(scoresAlcohol1))
+print ("Median of 3-4 scores")
+print (np.median(scoresAlcohol3))
+print ("Median of 5-7 scores")
+print (np.median(scoresAlcohol5))
+
+print ("Mean of 0-0 scores")
+print (np.mean(scoresCigs0))
+print ("Mean of 1-10 scores")
+print (np.mean(scoresCigs1))
+print ("Mean of 11-20 scores")
+print (np.mean(scoresCigs11))
+print ("Mean of 21-40 scores")
+print (np.mean(scoresCigs21))
+
+print ("Median of 0-0 scores")
+print (np.median(scoresCigs0))
+print ("Median of 1-10 scores")
+print (np.median(scoresCigs1))
+print ("Median of 11-20 scores")
+print (np.median(scoresCigs11))
+print ("Median of 21-40 scores")
+print (np.median(scoresCigs21))
+
+print ("Mean of Inactive scores")
+print (np.mean(scoresInactive))
+print ("Mean of Active scores")
+print (np.mean(scoresActive))
+print ("Mean of Very Active scores")
+print (np.mean(scoresVeryActive))
+
+print ("Median of Inactive scores")
+print (np.median(scoresInactive))
+print ("Median of Active scores")
+print (np.median(scoresActive))
+print ("Median of Very Active scores")
+print (np.median(scoresVeryActive))
+
+print (min(unitsCigs))
+print (max(unitsCigs))
+
+#_______________________________________________________________________
+
+from pylab import plt
+
+#Kmeans
+age = list(map(float, unitsCigs))
+weight = list(map(float, healthScore))
+#[age, weight, height, iq, unitsAlcohol, unitsCigs]
+kMeansArray = [age, weight]
+kmeans = KMeans(n_clusters=2) # initialization
+kmeans.fit(kMeansArray) # actual execution
+y_kmeans = kmeans.predict(kMeansArray)
+centers = kmeans.cluster_centers_
+print(centers)
+
+#plt.scatter(kMeansArray[0], kMeansArray[1])
+
+#++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
+
+print(len(age))
+print(len(weight))
+print(len(height))
+print(len(iq))
+print(len(unitsAlcohol))
+print(len(unitsCigs))
+print(len(scoresNumberActivity))
+print(len(genderNumberScores))
+print(len(healthScore))
+
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+from sklearn import datasets, linear_model
+
+tempAge = age
+tempWeight = weight
+tempHeight = height
+tempIq = iq
+tempUnitsAlcohol = unitsAlcohol
+tempUnitsCigs = unitsCigs
+tempScoresNumberActivity = scoresNumberActivity
+tempGenderNumberScores = genderNumberScores
+
+for index, item in enumerate(age):
+    item = (item-min(tempAge))/(max(tempAge)-min(tempAge))
+    
+for index, item in enumerate(weight):
+    item = (item-min(tempWeight))/(max(tempWeight)-min(tempWeight))
+    
+for index, item in enumerate(height):
+    item = (item-min(tempHeight))/(max(tempHeight)-min(tempHeight))
+    
+for index, item in enumerate(iq):
+    item = (item-min(tempIq))/(max(tempIq)-min(tempIq))
+    
+for index, item in enumerate(unitsAlcohol):
+    item = (item-min(tempUnitsAlcohol))/(max(tempUnitsAlcohol)-min(tempUnitsAlcohol))
+    
+for index, item in enumerate(unitsCigs):
+    item = (item-min(tempUnitsCigs))/(max(tempUnitsCigs)-min(tempUnitsCigs))
+    
+for index, item in enumerate(tempScoresNumberActivity):
+    item = (item-min(tempScoresNumberActivity))/(max(tempScoresNumberActivity)-min(tempScoresNumberActivity))
+    
+for index, item in enumerate(tempGenderNumberScores):
+    item = (item-min(tempGenderNumberScores))/(max(tempGenderNumberScores)-min(tempGenderNumberScores))
+    
+
+x = np.array([age, genderNumberScores,  weight, height, iq, unitsAlcohol, unitsCigs, scoresNumberActivity])
+y = np.array([healthScore])
+
+print(x.shape)
+print(y.shape)
+
+x = np.transpose(x)
+y = np.transpose(y)
+
+print(x)
+print(y)
+
+print(x.shape)
+print(y.shape)
+
+predict_value = x
+
+# Create linear regression object
+regr = linear_model.LinearRegression()
+regr.fit(x, y)
+predict_outcome = regr.predict(predict_value)
+result = {}
+result['intercept'] = regr.intercept_
+result['coefficient'] = regr.coef_
+result['predicted_value'] = predict_outcome
+
+print ("Intercept value " , result['intercept'])
+print ("coefficient" , result['coefficient'])
+print ("Predicted value: ",result['predicted_value'])
+
+#Read file in
+predictArray = np.genfromtxt('Population.csv', delimiter=',', dtype=None).astype(str)
+
+#Extract variables from raw input
+ageP = [predictArray[i][1] for i in range(len(predictArray))]
+sexP = [predictArray[i][2] for i in range(len(predictArray))]
+weightP = [predictArray[i][3] for i in range(len(predictArray))]
+heightP = [predictArray[i][4] for i in range(len(predictArray))]
+iqP = [predictArray[i][5] for i in range(len(predictArray))]
+unitsAlcoholP = [predictArray[i][6] for i in range(len(predictArray))]
+unitsCigsP = [predictArray[i][7] for i in range(len(predictArray))]
+activityP = [predictArray[i][8] for i in range(len(predictArray))]
+
+#strip off column labels
+del ageP[0]
+del sexP[0]
+del weightP[0]
+del heightP[0]
+del iqP[0]
+del unitsAlcoholP[0]
+del unitsCigsP[0]
+del activityP[0]
+
+#print(activityP)
+
+#convert to ints where appropriate
+ageP = list(map(int, ageP))
+weightP = list(map(int, weightP))
+heightP = list(map(int, heightP))
+iqP = list(map(int, iqP))
+unitsAlcoholP = list(map(int, unitsAlcoholP))
+unitsCigsP = list(map(int, unitsCigsP))
+
+genderNumberScoresP = []
+scoresNumberActivityP = []
+
+#extract female and male scores
+for index, item in enumerate(sexP):
+    if item == "Female":
+        genderNumberScoresP.append(0)
+    elif item == "Male":
+        genderNumberScoresP.append(1)
+        
+ #extract activity scores
+for index, item in enumerate(activityP):
+    if item == "Inactive":
+        scoresNumberActivityP.append(0)
+    elif item == "Active":
+        scoresNumberActivityP.append(1)
+    elif item == "Very Active":
+        scoresNumberActivityP.append(2)
+        
+for index, item in enumerate(ageP):
+    item = (item-min(tempAge))/(max(tempAge)-min(tempAge))
+    
+for index, item in enumerate(weightP):
+    item = (item-min(tempWeight))/(max(tempWeight)-min(tempWeight))
+    
+for index, item in enumerate(heightP):
+    item = (item-min(tempHeight))/(max(tempHeight)-min(tempHeight))
+    
+for index, item in enumerate(iqP):
+    item = (item-min(tempIq))/(max(tempIq)-min(tempIq))
+    
+for index, item in enumerate(unitsAlcoholP):
+    item = (item-min(tempUnitsAlcohol))/(max(tempUnitsAlcohol)-min(tempUnitsAlcohol))
+    
+for index, item in enumerate(unitsCigsP):
+    item = (item-min(tempUnitsCigs))/(max(tempUnitsCigs)-min(tempUnitsCigs))
+    
+for index, item in enumerate(scoresNumberActivityP):
+    item = (item-min(tempScoresNumberActivity))/(max(tempScoresNumberActivity)-min(tempScoresNumberActivity))
+    
+for index, item in enumerate(genderNumberScoresP):
+    item = (item-min(tempGenderNumberScores))/(max(tempGenderNumberScores)-min(tempGenderNumberScores))
+            
+predictData = np.array([ageP, genderNumberScoresP, weightP, heightP, iqP, unitsAlcoholP, unitsCigsP, scoresNumberActivityP])
+predictData = np.transpose(predictData)
+
+# Create linear regression object
+regrP = linear_model.LinearRegression()
+regrP.fit(x, y)
+predict_outcomeP = regrP.predict(predictData)
+resultP = {}
+resultP['predicted_value'] = predict_outcomeP
+print ("Predicted value: ",resultP['predicted_value'])
+
+print(predictData)
+
+#show_linear_line(x,y)
+
+#plt.scatter(centers[:, 0], centers[:, 1], c='black', s=200, alpha=0.5);
 
 #print (age)
 #print (sex)
